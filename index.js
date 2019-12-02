06df52c66a4cd29edfaa84b6aefb282a7ac8ba39
Wes Todd <wes@wesleytodd.com>
2019-12-01 16:10:06 -0800

    feat: refactor on @pkgjs/create BREAKING CHANGE

--- index.js
+++ index.js
@@ -1,54 +1,56 @@
 'use strict'
+const create = require('@pkgjs/create')
 const createPackageJson = require('create-package-json')
 const createGit = require('create-git')
 const cptmpl = require('cptmpl')
+
 const path = require('path')
+function tmplPath (...args) {
+  return path.join(__dirname, 'templates', ...args)
+}
 
-module.exports = async function createPackage (input = {}) {
-  // We need this for the defaults
-  const cwd = process.cwd()
-
-  // Removed undefined values from input and default some options
-  const options = Object.keys(input).reduce((o, key) => {
-    if (typeof input[key] !== 'undefined') {
-      o[key] = input[key]
+module.exports = create({
+  commandDescription: 'Scaffold a package',
+  uses: [createPackageJson, createGit],
+  options: {
+    remoteOrigin: {
+      type: 'string',
+      flag: {
+        key: 'remote-origin',
+        alias: 'o'
+      },
+      prompt: false
     }
-    return o
-  }, {
-    extended: false
-  })
-
-  // Defaults
-  let opts = Object.assign({
-    directory: cwd,
-    silent: false,
-    noPrompt: true
-  }, options)
-
-  await cptmpl(path.join(__dirname, 'templates', '.npmrc'), path.join(opts.directory, '.npmrc'))
-
-  const pkg = await createPackageJson({
-    directory: opts.directory,
-    silent: opts.silent,
-    extended: opts.extended,
-    noPrompt: opts.noPrompt,
-    name: opts.name,
-    scope: '@wesleytodd',
+  }
+}, async function createPackage (initOpts, input) {
+  const opts = await initOpts({
     version: '0.0.0',
     devDependencies: ['standard', 'mocha'],
+    scriptsTest: 'standard && mocha',
+    scriptsPreVersion: 'npm t',
+    scriptsPostPublish: 'git push origin && git push origin --tags',
+    ignoreTemplates: ['Node.gitignore', 'Global/macOS.gitignore'],
+    additionalRules: ['package-lock.json']
+  }, {
+    // Git options
+    initialCommitMessage: '',
+
+    // package.json options
     scripts: {
-      test: 'standard && mocha',
-      postpublish: 'git push origin && git push origin --tags'
+      debug: 'mocha --inspect --inspect-brk'
     }
   })
 
-  await cptmpl.recursive(path.join(__dirname, 'templates'), opts.directory, Object.assign({}, opts, pkg))
-
+  await cptmpl(tmplPath('.npmrc'), path.join(opts.directory, '.npmrc'))
+  const pkg = await createPackageJson(opts)
   await createGit({
-    directory: opts.directory,
-    silent: opts.silent,
-    extended: opts.extended,
-    noPrompt: opts.noPrompt,
+    ...opts,
+    initialCommitMessage: '',
     remoteOrigin: pkg.repository && pkg.repository.url
   })
-}
+
+  await cptmpl.recursive(tmplPath(), opts.directory, {
+    ...opts,
+    ...pkg
+  })
+})
--- test/index.js
+++ test/index.js
@@ -1,6 +1,5 @@
 'use strict'
-const { describe, it, beforeEach } = require('mocha')
-const assert = require('assert')
+const { suite, test, beforeEach } = require('mocha')
 const path = require('path')
 const fs = require('fs-extra')
 const pkg = require('../package.json')
@@ -8,18 +7,15 @@ const createPackage = require('..')
 
 const TMP_DIR = path.join(__dirname, 'fixtures', 'tmp')
 
-describe(pkg.name, () => {
+suite(pkg.name, () => {
   beforeEach(() => fs.remove(TMP_DIR))
 
-  it('work...', async function () {
+  test('scaffolds a package', async function () {
     this.timeout(0)
     await createPackage({
+      prompt: false,
       silent: true,
-      noPropmt: true,
-      name: 'test',
       directory: TMP_DIR
     })
-
-    assert(true)
   })
 })
